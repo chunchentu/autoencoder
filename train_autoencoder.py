6b5477333f808e9a3bd343c1431121d2da1240c1
Chun-Chen Tu <timtu@umich.edu>
2017-10-29 03:50:02 -0400

    supporting data generator for training imagenet

--- train_autoencoder.py
+++ train_autoencoder.py
@@ -11,8 +11,10 @@ from setup_mnist import MNIST
 from setup_cifar import CIFAR
 from setup_inception import ImageNet
 import tensorflow as tf
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 
-def train(data, compressMode=1, batch_size=1000, epochs=1000, saveFilePrefix=None, use_tanh=True, train_imagenet=False):
+
+def train(data, compressMode=1, batch_size=1000, epochs=1000, saveFilePrefix=None, use_tanh=True, train_imagenet=False, imagenet_path=None):
 
     """Train autoencoder
 
@@ -26,16 +28,24 @@ def train(data, compressMode=1, batch_size=1000, epochs=1000, saveFilePrefix=Non
     # data.train_data may have different definition of the shape, need to check
     # for MNIST and CIFAR
 
-    trainNum, imgH, imgW, numChannels = data.train_data.shape
+    
 
-    x_train = data.train_data
-    y_train = x_train
+    if not train_imagenet:
+        trainNum, imgH, imgW, numChannels = data.train_data.shape
+        x_train = data.train_data
+        y_train = x_train
 
-    x_test = data.test_data
-    y_test = x_test
+        x_test = data.test_data
+        y_test = x_test
 
-    print("Shape of training data:{}".format(x_train.shape))
-    print("Shape of testing data:{}".format(x_test.shape))
+        print("Shape of training data:{}".format(x_train.shape))
+        print("Shape of testing data:{}".format(x_test.shape))
+    else:
+        train_datagen = data.train_datagen
+        test_datagen = data.test_datagen
+        imgH = 299
+        imgW = 299
+        numChannels = 3
 
 
     # build a neural network from the 1st layer to the last layer
@@ -206,12 +216,25 @@ def train(data, compressMode=1, batch_size=1000, epochs=1000, saveFilePrefix=Non
 
     checkpointer = ModelCheckpoint(filepath=ckptFileName, verbose=1, save_best_only=True)
 
-    decoder_model.fit(x_train, y_train,
-              batch_size=batch_size,
-              validation_data=(x_test, y_test),
-              epochs=epochs,
-              shuffle=True,
-              callbacks = [checkpointer])
+
+    if not train_imagenet:
+        decoder_model.fit(x_train, y_train,
+                  batch_size=batch_size,
+                  validation_data=(x_test, y_test),
+                  epochs=epochs,
+                  shuffle=True,
+                  callbacks = [checkpointer])
+    else:
+        print("Train imagenet with data generater")
+        
+
+        decoder_model.fit_generator(
+                    train_datagen,
+                    steps_per_epoch= 50,
+                    epochs=epochs,
+                    validation_data=test_datagen,
+                    validation_steps=100,
+                    callbacks = [checkpointer])
 
     print("Checkpoint is saved to {}\n".format(ckptFileName))
 
@@ -234,6 +257,12 @@ def train(data, compressMode=1, batch_size=1000, epochs=1000, saveFilePrefix=Non
     decoder_model.save_weights(decoder_weight_filename)
     print("Decoder weight is saved to {}\n".format(decoder_weight_filename))
 
+# a small class for imagenet image image generator
+class imagenet_imageGen:
+    def __init__(self, train_datagen, test_datagen):
+        self.train_datagen = train_datagen
+        self.test_datagen = test_datagen
+
 
 def main(args):
     # load data
@@ -243,13 +272,37 @@ def main(args):
     elif args["dataset"] == "cifar10":
         data = CIFAR()
     elif args["dataset"] == "imagenet":
-        data = ImageNet(datasetSize=args["imagenet_data_size"], testRatio=0.1)
+        # the following code is the old function for loading imagenet data
+        # data = ImageNet(datasetSize=args["imagenet_data_size"], testRatio=0.1)
         
+        # new version uses ImageDataGenerator provided by Keras
+        train_datagen = ImageDataGenerator(
+                            rescale=1./255,
+                            shear_range=0.2,
+                            zoom_range=0.2,
+                            horizontal_flip=True,
+                            fill_mode='nearest')
+        test_datagen = ImageDataGenerator(rescale=1./255)
+        train_generator = train_datagen.flow_from_directory(
+                                "../imagenetdata/train_dir",
+                                target_size=(299, 299),  
+                                batch_size=args["batch_size"],
+                                class_mode="input")  
+
+        # this is a similar generator, for validation data
+        validation_generator = test_datagen.flow_from_directory(
+                                "../imagenetdata/test_dir",
+                                target_size=(299, 299),
+                                batch_size=args["batch_size"],
+                                class_mode="input")
+        data = imagenet_imageGen(train_generator, validation_generator)
+        print(data.train_datagen)
     print("Done...")
 
     print("Start training autoencoder")
-    train(data, compressMode=args["compress_mode"], batch_size=args["batch_size"], epochs=args["epochs"],
-          saveFilePrefix=args["save_prefix"], use_tanh=args["use_tanh"], train_imagenet=args["train_imagenet"])
+    train(data, compressMode=args["compress_mode"], batch_size=args["batch_size"], 
+            epochs=args["epochs"], saveFilePrefix=args["save_prefix"], 
+            use_tanh=args["use_tanh"], train_imagenet=args["train_imagenet"])
 
 
 if __name__ == "__main__":
@@ -264,6 +317,7 @@ if __name__ == "__main__":
     parser.add_argument("--train_imagenet", action='store_true', help = "the encoder for imagenet would be different")
     parser.add_argument("--imagenet_data_size", type=int,  default=10000, help="the size of imagenet loaded for training, Max 50,000")
     parser.add_argument("--use_tanh", action='store_true', help = "use tanh as activation function")
+    parser.add_argument("--imagenet_path", help="the path to imagenet images")
     args = vars(parser.parse_args())
     if not os.path.isdir("model"):
         print("Folder for saving models does not exist. The folder is created.")
